df9098618eccf384a3fd739c22890df64b622e28
chaitanya <chaitanya@gmail.com>
2026-01-12 19:23:28 +0530

    intial commit with terraform code

---
+++ vpc.tf
@@ -0,0 +1,31 @@
+resource "aws_vpc" "DemoBasicVPC" {
+  cidr_block           = "10.25.0.0/16"
+  enable_dns_hostnames = true
+  tags = {
+    "Name" = "DevTFVPC"
+  }
+}
+
+resource "aws_internet_gateway" "DemoBasicIGW" {
+  vpc_id = aws_vpc.DemoBasicVPC.id
+  tags = {
+    "Name" = "DevTFIGW"
+  }
+}
+
+resource "aws_subnet" "DemoSubnet-1" {
+  vpc_id = aws_vpc.DemoBasicVPC.id
+  cidr_block = "10.25.1.0/24"
+  availability_zone = "ap-south-1a"
+  tags = {
+    "Name" = "DevTFSubnet-1"
+  }
+}
+resource "aws_subnet" "DemoSubnet-2" {
+  vpc_id = aws_vpc.DemoBasicVPC.id
+  cidr_block = "10.25.2.0/24"
+  availability_zone = "ap-south-1b"
+  tags = {
+    "Name" = "DevTFSubnet-2"
+  }
+}
