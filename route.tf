200adf2d588837481aab6b063a3306fd49717536
chaitanya <chaitanya@gmail.com>
2026-01-12 19:35:45 +0530

    added route table

---
+++ route.tf
@@ -0,0 +1,10 @@
+resource "aws_route_table" "DemoBasicRoute" {
+  vpc_id = aws_vpc.DemoBasicVPC.id
+  tags = {
+    "Name" = "DevRoute"
+  }
+  route {
+    gateway_id = aws_internet_gateway.DemoBasicIGW.id
+    cidr_block = "0.0.0.0/0"
+  }
+}
